c101e281f4cb6f2e4852079fdc6077e9b06d5ea5
ad-boop <AF908@live.mdx.ac.uk>
2023-01-09 19:03:15 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+var app = express();
+
+let lessons=[
+    {'topic':'maths','location':'Hendon','price':100},
+    {'topic':'maths','location':'Colindale','price':80},
+    {'topic':'maths','location':'Brent Cross','price':90},
+    {'topic':'maths','location':'Golders Green','price':120}]
+
+// Routes GET requests to /lessons to the request handler
+app.get("/lessons", function(request, response) {    
+    response.send(lessons);
+});
+
+let user={'email':'user@email.com','password':'mypassword'};
+app.get("/user", function(request, response) {    
+    response.send(user);
+});
+
+
+// Starts the server on port 3000
+app.listen(3000);
